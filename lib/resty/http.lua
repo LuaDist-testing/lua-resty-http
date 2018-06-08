7ee798daf5fe16eded6b5e5104213e4734988cd0
LunaCI <lunaci@luadist.org>
2018-06-08 01:33:29 +0000

    Update to version 0.11-0

--- lib/resty/http.lua
+++ lib/resty/http.lua
@@ -1,5 +1,6 @@
 local http_headers = require "resty.http_headers"
 
+local ngx = ngx
 local ngx_socket_tcp = ngx.socket.tcp
 local ngx_req = ngx.req
 local ngx_req_socket = ngx_req.socket
@@ -21,6 +22,7 @@ local ngx_DEBUG = ngx.DEBUG
 local ngx_ERR = ngx.ERR
 local ngx_var = ngx.var
 local ngx_print = ngx.print
+local ngx_header = ngx.header
 local co_yield = coroutine.yield
 local co_create = coroutine.create
 local co_status = coroutine.status
@@ -76,8 +78,27 @@ local co_wrap = function(func)
 end
 
 
+-- Returns a new table, recursively copied from the one given.
+--
+-- @param   table   table to be copied
+-- @return  table
+local function tbl_copy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == "table" then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[tbl_copy(orig_key)] = tbl_copy(orig_value)
+        end
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+
+
 local _M = {
-    _VERSION = '0.10',
+    _VERSION = '0.11',
 }
 _M._USER_AGENT = "lua-resty-http/" .. _M._VERSION .. " (Lua) ngx_lua/" .. ngx.config.ngx_lua_version
 
@@ -222,7 +243,7 @@ function _M.parse_uri(self, uri, query_in_path)
         -- If the URI is schemaless (i.e. //example.com) try to use our current
         -- request scheme.
         if not m[1] then
-            local scheme = ngx.var.scheme
+            local scheme = ngx_var.scheme
             if scheme == "http" or scheme == "https" then
                 m[1] = scheme
             else
@@ -713,6 +734,7 @@ end
 
 
 function _M.request(self, params)
+    params = tbl_copy(params)  -- Take by value
     local res, err = self:send_request(params)
     if not res then
         return res, err
@@ -723,6 +745,8 @@ end
 
 
 function _M.request_pipeline(self, requests)
+    requests = tbl_copy(requests)  -- Take by value
+
     for _, params in ipairs(requests) do
         if params.headers and params.headers["Expect"] == "100-continue" then
             return nil, "Cannot pipeline request specifying Expect: 100-continue"
@@ -765,7 +789,7 @@ end
 
 
 function _M.request_uri(self, uri, params)
-    if not params then params = {} end
+    params = tbl_copy(params or {})  -- Take by value
 
     local parsed_uri, err = self:parse_uri(uri, false)
     if not parsed_uri then
@@ -868,7 +892,7 @@ function _M.proxy_response(self, response, chunksize)
     -- Filter out hop-by-hop headeres
     for k,v in pairs(response.headers) do
         if not HOP_BY_HOP_HEADERS[str_lower(k)] then
-            ngx.header[k] = v
+            ngx_header[k] = v
         end
     end
 
--- lua-resty-http-0.11-0.rockspec
+++ lua-resty-http-0.11-0.rockspec
@@ -1,16 +1,16 @@
 -- This file was automatically generated for the LuaDist project.
 
 package = "lua-resty-http"
-version = "0.10-0"
+version = "0.11-0"
 -- LuaDist source
 source = {
-  tag = "0.10-0",
+  tag = "0.11-0",
   url = "git://github.com/LuaDist-testing/lua-resty-http.git"
 }
 -- Original source
 -- source = {
 --    url = "git://github.com/pintsized/lua-resty-http",
---    tag = "v0.10"
+--    tag = "v0.11"
 -- }
 description = {
    summary = "Lua HTTP client cosocket driver for OpenResty / ngx_lua.",
